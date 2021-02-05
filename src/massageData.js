09ce1d2f2d86eaf1a45b1a288da2e5b8eeeb8a49
Tyler Martin <tyler.martin@electric-playground.com>
2021-02-04 21:36:17 -0400

    adds google sheets api functionality

--- src/massageData.js
+++ src/massageData.js
@@ -1,4 +1,7 @@
-const results = require ('../games.json');
+const games = require ('../games.json');
+const odds = require ('../odds.json');
+const spreadsheetId = require('../private/spreadsheet');
+const { google } = require('googleapis');
 
 /**
  * 
@@ -7,13 +10,54 @@ const results = require ('../games.json');
  * 2. over/under
  * 3. Asian Handicap (1.5) spread
  * 
- * response is an array of objects
+ * games is an array of objects
  * each object is a game, which team information
+ * odds is just one event with many odds from many bookkeepers
  */
 const massageData = (data) => {
-  console.log(data.response[0]);
-  // data = JSON.parse(results);
-  // console.log(data);
+  let massagedData = [
+    ['Bookmaker'],
+    ['Visitor'],
+    ['Home']
+  ]
+
+  data.response[0].bookmakers.forEach(bookmaker => {
+    massagedData[0].push(bookmaker.name);
+    bookmaker.bets.forEach(bet => {
+      if (bet.name === 'Home/Away') {
+        massagedData[1].push(bet.value[1].odd)
+        massagedData[2].push(bet.value[0].odd)
+      }
+    });
+    // console.log(bookmaker.bets[0].name);
+  });
+  return massagedData;
 };
 
-massageData(results);
+// massageData(odds);
+
+/**
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+module.exports = (auth) => {
+  let range = 'new!A1';
+  let values = massageData(odds);
+  let resource = {
+    values,
+  };
+  let valueInputOption = 'RAW';
+
+  let request = {
+    spreadsheetId,
+    range,
+    valueInputOption,
+    resource,
+  }
+
+  const sheets = google.sheets({version: 'v4', auth});
+  try {
+    sheets.spreadsheets.values.update(request);
+  } catch (error) {
+    console.log(error);
+  }
+}
