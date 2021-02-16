9c27b91c711ad02a506c021164dc3fa07e52c46e
Tyler Martin <tyler.martin@electric-playground.com>
2021-02-15 23:11:23 -0400

    refactoring formatData function, using start.js to retrieve and write data

--- start.js
+++ start.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const _ = require('./lib/index');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,11 +12,15 @@ const selections = {
   spread: null,
 };
 
+/** Possible options existing in the odds data returned from API
+ * 'Handicap Result'
+ * 'Asian Handicap'
+ */
 const options = {
-  '1': '3-Way',
-  '2': 'Moneyline',
-  '3': 'Total',
-  '4': 'Spread',
+  '1': '3Way Result',
+  '2': 'Home/Away', // Moneyline
+  '3': 'Over/Under', //Total
+  '4': 'Asian Handicap', //Spread
 };
 
 getValue = function(value) {
@@ -24,16 +29,26 @@ getValue = function(value) {
     endTransmission();
   });
 }
-endTransmission = function() {
+endTransmission = async function() {
   console.log('retrieving and writing odds...', selections);
+
+  // call _.api to get a list of games for today
+  // call _.api to return odds info
+  // call _.formatData to return the data to write
+  // call _.write to write to the spreadsheet
+
+  let data = await _.api();
+  let formattedData = _.formatData(data, selections);
+  console.log('will write: ', formattedData);
+  // _.writeToSheet(auth, formattedData);
   rl.close()
 }
 
-rl.question("Select a Bet Type (1,2, 3, or 4):\n 1. 3-way\n 2. Moneyline\n 3. Total\n 4. Spread\n 4. Spread\n", (answer) => {
+rl.question("Select a Bet Type (1,2, 3, or 4):\n 1. 3-way\n 2. Moneyline\n 3. Total\n 4. Spread\n", (answer) => {
   selections.type = options[answer];
-  if (answer == 2) {
+  if (answer == 3) {
     getValue('total');
-  } else if (answer == 3) {
+  } else if (answer == 4) {
     getValue('spread');
   } else {
     endTransmission();
