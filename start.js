a0767064d50174dbc36b2316e0d07f6b74384a28
Tyler Martin <tyler.martin@electric-playground.com>
2021-05-24 17:52:00 -0300

    using Promise to wait for api call:

--- start.js
+++ start.js
@@ -6,10 +6,13 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+let api = new _.Api();
+
 const selections = {
-  league: null,
+  sport: null,
   game: null,
   bet: null,
+  region: 'us',
 };
 
 const options = {
@@ -17,48 +20,49 @@ const options = {
   '2': 'totals',
   '3': 'spreads', 
 };
-const leagues = {
+const sports = {
   '1': 'icehockey_nhl',
   '2': 'basketball_nba',
   '3': 'americanfootball_nfl',
   '4': 'baseball_mlb',
 }
 
-// TODO: first we need to allow selection of a sport, then a game
 rl.question('Select a League (1, 2, 3, or 4):\n1. NHL\n2. NBA\n3. NFL\n4. MLB\n', (answer) => {
-  selections.league = leagues[answer];
-    getBetSelection();    
+  selections.sport = sports[answer];
+  getBetSelection();    
 })
 
 getBetSelection = function() {
   rl.question('Select a Bet Type (1,2, or 3):\n 1. Moneyline\n 2. Total\n 3. Spread\n', (answer) => {
     selections.bet = options[answer];
-    let api = new _.Api()
-    await api.setTempEvents(); //api.getOdds({}) eventually
-    getGameSelection(api.getEventList());
-    // endTransmission();
+
+    // let wait = Promise.resolve(api.setTempEvents()); //this is where we will call the api
+    let wait = Promise.resolve(api.getOdds({
+      sport: selections.sport,
+      region: selections.region,
+      mkt: selections.bet
+    })); //this is where we will call the api
+    wait.then(() => {
+      getGameSelection(api.getEventList());
+    })
+
   });
 }
 
-getGameSelection = function(list) {
-  rl.question('hey', (answer) => {
-    // print the list somehow
-    endTransmission();
+getGameSelection = function(question) {
+  rl.question(question, (answer) => {
+    selections.game = answer;
+    endTransmission(answer);
   });
 }
-endTransmission = async function() {
+endTransmission = async function(index) {
   console.log('retrieving and writing odds...', selections);
 
-  // call _.api to get a list of games for today
-  // call _.api to return odds info
+  // get api event 
   // call _.formatData to return the data to write
   // call _.write to write to the spreadsheet
 
-  // let data = await _.api.SOMETHING();
-  let api = new _.Api()
-  await api.setTempEvents();
-  // console.log(api.events);
-  let data = api.events[1];
+  let data = api.events[index-1];
   let formattedData = _.formatData(data, selections);
   _.writeToSheet(_.returnAuth(), formattedData);
   rl.close()
